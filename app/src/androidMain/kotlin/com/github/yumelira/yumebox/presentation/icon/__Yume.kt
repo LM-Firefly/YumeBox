cfd9f0f03708e24db163b5ba8c389350f91c0821
Yume <Yume@YumeYuka.moe>
2024-08-05 18:06:19 +0000

    feat(profiles): add link management functionality and UI components
    Implement link management features including adding, editing, and deleting links.
    Introduce UI components for link settings and sharing options.
    Enhance user experience by allowing users to choose how links are opened
    (in-app or external browser) and set default links for quick access.

--- app/src/androidMain/kotlin/com/github/yumelira/yumebox/presentation/icon/__Yume.kt
+++ app/src/androidMain/kotlin/com/github/yumelira/yumebox/presentation/icon/__Yume.kt
@@ -1,31 +1,8 @@
 package com.github.yumelira.yumebox.presentation.icon
 
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.github.yumelira.yumebox.presentation.icon.yume.Atom
-import com.github.yumelira.yumebox.presentation.icon.yume.Bolt
-import com.github.yumelira.yumebox.presentation.icon.yume.Chromium
-import com.github.yumelira.yumebox.presentation.icon.yume.Github
-import com.github.yumelira.yumebox.presentation.icon.yume.House
-import com.github.yumelira.yumebox.presentation.icon.yume.Link
-import com.github.yumelira.yumebox.presentation.icon.yume.Meta
-import com.github.yumelira.yumebox.presentation.icon.yume.Play
-import com.github.yumelira.yumebox.presentation.icon.yume.Rocket
-import com.github.yumelira.yumebox.presentation.icon.yume.Sparkles
-import com.github.yumelira.yumebox.presentation.icon.yume.Square
-import com.github.yumelira.yumebox.presentation.icon.yume.Substore
-import com.github.yumelira.yumebox.presentation.icon.yume.Zap
-import com.github.yumelira.yumebox.presentation.icon.yume.Zashboard
-import com.github.yumelira.yumebox.presentation.icon.yume.`Arrow-down-up`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Chart-column`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Git-merge`
-import com.github.yumelira.yumebox.presentation.icon.yume.`List-chevrons-up-down`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Package-check`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Redo-dot`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Scan-eye`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Scroll-text`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Settings-2`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Squares-exclude`
-import com.github.yumelira.yumebox.presentation.icon.yume.`Wifi-cog`
+import com.github.yumelira.yumebox.presentation.icon.yume.`Badge-plus`
+import com.github.yumelira.yumebox.presentation.icon.yume.`Circle-fading-arrow-up`
 import kotlin.String
 import kotlin.collections.List as ____KtList
 import kotlin.collections.Map as ____KtMap
@@ -39,10 +16,7 @@ public val Yume.AllIcons: ____KtList<ImageVector>
     if (__AllIcons != null) {
       return __AllIcons!!
     }
-    __AllIcons= listOf(`Arrow-down-up`, Atom, Bolt, `Chart-column`, Chromium, `Git-merge`, Github,
-        House, Link, `List-chevrons-up-down`, Meta, `Package-check`, Play, `Redo-dot`, Rocket,
-        `Scan-eye`, `Scroll-text`, `Settings-2`, Sparkles, Square, `Squares-exclude`, Substore,
-        `Wifi-cog`, Zap, Zashboard)
+    __AllIcons= listOf(`Badge-plus`, `Circle-fading-arrow-up`)
     return __AllIcons!!
   }
 
@@ -53,13 +27,7 @@ public val Yume.AllIconsNamed: ____KtMap<String, ImageVector>
     if (__AllIconsNamed != null) {
       return __AllIconsNamed!!
     }
-    __AllIconsNamed= mapOf("arrow-down-up" to `Arrow-down-up`, "atom" to Atom, "bolt" to Bolt,
-        "chart-column" to `Chart-column`, "chromium" to Chromium, "git-merge" to `Git-merge`,
-        "github" to Github, "house" to House, "link" to Link, "list-chevrons-up-down" to
-        `List-chevrons-up-down`, "meta" to Meta, "package-check" to `Package-check`, "play" to Play,
-        "redo-dot" to `Redo-dot`, "rocket" to Rocket, "scan-eye" to `Scan-eye`, "scroll-text" to
-        `Scroll-text`, "settings-2" to `Settings-2`, "sparkles" to Sparkles, "square" to Square,
-        "squares-exclude" to `Squares-exclude`, "substore" to Substore, "wifi-cog" to `Wifi-cog`,
-        "zap" to Zap, "zashboard" to Zashboard)
+    __AllIconsNamed= mapOf("badge-plus" to `Badge-plus`, "circle-fading-arrow-up" to
+        `Circle-fading-arrow-up`)
     return __AllIconsNamed!!
   }
